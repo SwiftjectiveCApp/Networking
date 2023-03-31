41a5bb7599e071fd73222828858b4eb52eed99a9
Khoa Le <ldakhoa1308@gmail.com>
2023-03-31 17:21:49 +0700

    Update

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
             targets: ["Networking", "UserSession"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/tphduy/Networkable", from: "2.0.0"),
-         .package(name: "Models", path: "../Models")
+        .package(url: "https://github.com/tphduy/Networkable", from: "2.0.0"),
+        .package(url: "https://github.com/SwiftjectiveCApp/Models", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
